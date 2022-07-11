528d7f62fd90e29de9c8b63a53590d172c558db1
ksenia_subbotina <ks@didlogic.com>
2022-07-11 14:09:05 +0300

    Add selector

---
+++ src/NewEditor/components/Nodes/Node.tsx
@@ -0,0 +1,42 @@
+import React from "react"
+import classNames from "classnames"
+import { Vector2d } from "../../../geometry"
+import { Node as NodeType } from "../../../types"
+import { BUTTON_LEFT } from "../../constants"
+import {useSetRecoilState} from "recoil";
+import { selectedNodeState } from "../../ducks/store"
+
+const nodeStyle = (pos: Vector2d) => ({
+    top: `${pos.y}px`,
+    left: `${pos.x}px`
+})
+
+type NodeProps = {
+    node: NodeType
+}
+
+const Node: React.FC<NodeProps> = ({ node }) => {
+    const nodeClassNames = classNames(
+        "node",
+        node.classNames || []
+    )
+
+    const onDragStarted = (node: NodeType, e: React.MouseEvent<HTMLElement>) => {
+        const setSelectedNode = useSetRecoilState(selectedNodeState)
+
+        if (e.button === BUTTON_LEFT) {
+            setSelectedNode(node)
+        }
+    }
+
+    return (
+        <div
+            onMouseDown={(e) => onDragStarted(node, e)}
+            style={nodeStyle(node.position)}
+            className={nodeClassNames}>
+            {node.children}
+        </div>
+    )
+}
+
+export default Node
---
+++ src/NewEditor/components/Nodes/NodesContainer.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react"
+import { Node as NodeType } from "../../../types"
+import Node from "./Node"
+import { useRecoilValue } from "recoil"
+import { nodesState } from "../../ducks/store"
+
+type Transformation = {
+    dx: number
+    dy: number
+    zoom: number
+}
+
+const useNodeHooks = () => {
+    const [transformation, setTransformation] = useState<Transformation>({ dx: 0, dy: 0, zoom: 1 })
+
+    return { transformation, setTransformation }
+}
+
+export const NodeContainer: React.FC = () => {
+    const nodes = useRecoilValue(nodesState)
+
+    const { transformation, setTransformation } = useNodeHooks()
+
+    const nodesContainerStyle = {
+        transform: `matrix(${transformation.zoom},0,0,${transformation.zoom},${transformation.dx},${transformation.dy})`
+    }
+
+    console.log('node container', nodes)
+
+    return <div style={nodesContainerStyle}>
+        {nodes.map((node) => <Node node={node} key={node.id} />)}
+    </div>
+}
