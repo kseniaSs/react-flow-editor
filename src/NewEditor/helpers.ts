4e06f284f3eabea0338209e17f1d09c5ef59d1bf
ksenia_subbotina <ks@didlogic.com>
2022-07-15 15:10:35 +0300

    Add connection

--- src/NewEditor/helpers.ts
+++ src/NewEditor/helpers.ts
@@ -5,7 +5,8 @@ export const resetEvent = (e: React.MouseEvent<HTMLElement>) => {
   e.preventDefault()
 }
 
-export const inNode = (inputPosition: Vector2d, nodePosition: DOMRect): boolean => {
-  console.log(inputPosition, "input Position", nodePosition)
-  return false
-}
+export const inNode = (inputPosition: Vector2d, nodePosition: DOMRect): boolean =>
+  nodePosition.left < inputPosition.x &&
+  inputPosition.x < nodePosition.right &&
+  nodePosition.top < inputPosition.y &&
+  inputPosition.y < nodePosition.bottom
