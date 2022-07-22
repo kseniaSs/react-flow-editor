e8d1952f4b2ad1e3e5e6935b2522359263ce270e
ksenia_subbotina <ks@didlogic.com>
2022-07-22 09:54:03 +0300

    Add arrow to connection

--- src/NewEditor/components/Connections/Container.tsx
+++ src/NewEditor/components/Connections/Container.tsx
@@ -3,10 +3,11 @@ import { Connection } from "./Connection"
 import { useRecoilValue } from "recoil"
 import { nodesState } from "../../ducks/store"
 import { NewConnection } from "./NewConnection"
-import { Point } from "../../types"
+import { Point, Offset } from "../../types"
 
 type ContainerProps = {
   pointPosition: Point
+  offset: Offset
 }
 
 // TODO: Change to props
@@ -15,19 +16,25 @@ const Arrow: React.FC = () => (
     <marker
       id="triangle"
       viewBox="0 0 10 10"
-      refX="0"
+      refX="5"
       refY="5"
+      preserveAspectRatio="none"
       markerUnits="strokeWidth"
       markerWidth="10"
       markerHeight="8"
-      orient="auto-start-reverse"
+      stroke="none"
     >
-      <path d="M 0 0 L 10 5 L 0 10 z"></path>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M0.292896 0.29289C0.683422 -0.0976324 1.31659 -0.0976297 1.70711 0.292896L5.20674 3.79256C5.59727 4.18309 5.59726 4.81625 5.20674 5.20677L1.70711 8.7064C1.31658 9.09693 0.683417 9.09693 0.292893 8.7064C-0.0976311 8.31588 -0.0976311 7.68272 0.292893 7.29219L3.08542 4.49966L0.29289 1.7071C-0.0976324 1.31658 -0.0976297 0.683413 0.292896 0.29289Z"
+        fill="#3C7BE1"
+      />
     </marker>
   </defs>
 )
 
-export const Container: React.FC<ContainerProps> = ({ pointPosition }) => {
+export const Container: React.FC<ContainerProps> = ({ pointPosition, offset }) => {
   const nodes = useRecoilValue(nodesState)
 
   const connectionContainerStyle: React.CSSProperties = {
@@ -38,9 +45,9 @@ export const Container: React.FC<ContainerProps> = ({ pointPosition }) => {
     <svg className="connections" xmlns="http://www.w3.org/2000/svg" style={connectionContainerStyle}>
       <Arrow />
       {nodes.map((node) => (
-        <Connection pointPosition={pointPosition} key={`${node.id}-connection`} node={node} />
+        <Connection pointPosition={pointPosition} key={`${node.id}-connection`} offset={offset} node={node} />
       ))}
-      <NewConnection pointPosition={pointPosition} />
+      <NewConnection pointPosition={pointPosition} offset={offset} />
     </svg>
   )
 }
--- src/NewEditor/components/Connections/NewConnection.tsx
+++ src/NewEditor/components/Connections/NewConnection.tsx
@@ -2,13 +2,14 @@ import React from "react"
 import { useRecoilValue } from "recoil"
 import InputConnection from "./InputConnection"
 import { newConnectionState, nodesState, selectedNodeState } from "../../ducks/store"
-import { Point } from "../../types"
+import { Offset, Point } from "../../types"
 
 type NewConnectionProps = {
   pointPosition: Point
+  offset: Offset
 }
 
-export const NewConnection: React.FC<NewConnectionProps> = ({ pointPosition }) => {
+export const NewConnection: React.FC<NewConnectionProps> = ({ pointPosition, offset }) => {
   const newConnectionPosition = useRecoilValue(newConnectionState)
   const selectedNodeId = useRecoilValue(selectedNodeState)
   const nodes = useRecoilValue(nodesState)
@@ -17,12 +18,19 @@ export const NewConnection: React.FC<NewConnectionProps> = ({ pointPosition }) =
 
   const outputNode = nodes.find((node) => node.id === selectedNodeId)
 
+  const outputPosition = outputNode.rectPosition
+    ? {
+        x: outputNode.rectPosition.right - offset.offsetLeft + pointPosition.x,
+        y: outputNode.rectPosition.bottom - offset.offsetTop + pointPosition.y
+      }
+    : outputNode.position
+
   return (
     <InputConnection
       key={`${selectedNodeId}_new`}
       pointPosition={pointPosition}
       outputPosition={newConnectionPosition}
-      inputPosition={outputNode.position}
+      inputPosition={outputPosition}
     />
   )
 }
