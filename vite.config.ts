205811b4ea6a1a1ebe63ee5d5972f397bc8b6df8
Arseniy Suschevskiy <a.suschevskiy@voiso.com>
2022-09-22 18:49:25 +0300

    Add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@/*": path.resolve(__dirname, "./src")
+    }
+  },
   plugins: [
     react({ jsxRuntime: "classic" }),
     dts(),
