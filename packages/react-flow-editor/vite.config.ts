e51f947a59a2f4c5c0af0dbb410fa8d1d2e281ef
Arseniy Suschevskiy <a.suschevskiy@voiso.com>
2022-11-28 18:34:50 +0600

    correct publish workflow

--- packages/react-flow-editor/vite.config.ts
+++ packages/react-flow-editor/vite.config.ts
@@ -1,8 +1,9 @@
+import path from "path"
+
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import dts from "vite-plugin-dts"
 import { viteStaticCopy } from "vite-plugin-static-copy"
-import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
