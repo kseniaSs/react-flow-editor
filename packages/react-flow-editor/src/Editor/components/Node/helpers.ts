70a2ce8c52bd0735a8ad42bac56893e00b6f30fa
Roman Kaluger <r.kaluger@sftpro.ru>
2022-11-29 11:13:24 +0300

    add disabled state

--- packages/react-flow-editor/src/Editor/components/Node/helpers.ts
+++ packages/react-flow-editor/src/Editor/components/Node/helpers.ts
@@ -1,4 +1,5 @@
-import { NodeState, Point } from "@/types"
+import { nodeActions } from "@/Editor/state"
+import { NodeState, Point, Node, Output } from "@/types"
 
 export const nodeStyle = (pos: Point, state: NodeState | null) => ({
   transform: `translate(${pos.x}px, ${pos.y}px)`,
@@ -7,3 +8,16 @@ export const nodeStyle = (pos: Point, state: NodeState | null) => ({
 })
 
 export const buildDotId = (nodeId: string) => `dot-${nodeId}`
+
+const isNoNodeFreeInputs = (node: Node, nodes: Node[]): boolean =>
+  nodes.filter((curNode) => curNode.outputs.map((out) => out.nextNodeId).includes(node.id)).length === node.inputNumber
+
+const isSameNode = (connectableNode: Node, curNode: Node): boolean => curNode.id === connectableNode.id
+
+export const markDisabledNodes = (connectableNode: Node, nodes: Node[]) => {
+  const disabledNodesIds = nodes
+    .filter((curNode) => !isSameNode(connectableNode, curNode) && isNoNodeFreeInputs(curNode, nodes))
+    .map((curNode) => curNode.id)
+
+  nodeActions.changeNodesState(disabledNodesIds, NodeState.disabled)
+}
