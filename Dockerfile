ca4f195da59705282baf8e03317ddd1b417075e5
Roman Kaluger <r.kaluger@sftpro.ru>
2022-12-28 11:03:46 +0300

    docker compose works

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,16 @@
-FROM node:16.14.0 as install
+FROM node:16.14.0 as flow-editor-install
+
+WORKDIR /flow-editor
+ADD . /flow-editor
+RUN npm install -g pnpm
+RUN npm install -g vite
+RUN pnpm install
+
 FROM cypress/included:11.2.0 as cypress
 
 WORKDIR /flow-editor
-
-ADD . /flow-editor
-
+COPY --from=flow-editor-install /flow-editor /flow-editor
+COPY ./packages/e2e/cypress.config.ts /flow-editor
 RUN npm install -g pnpm
-RUN pnpm install
-
-RUN cd packages/examples && pnpm dev &
-RUN cd packages/e2e && pnpm run cypress:run
+ENV UNDER_DOCKER=true
 
--- packages/e2e/cypress.config.ts
+++ packages/e2e/cypress.config.ts
@@ -3,14 +3,14 @@ import { defineConfig } from "cypress"
 
 export default defineConfig({
   projectId: "XXX",
-  fileServerFolder: "./",
+  fileServerFolder: process.env.UNDER_DOCKER ? "./packages/e2e" : "./",
   pageLoadTimeout: 100000,
   modifyObstructiveCode: false,
   chromeWebSecurity: false,
   video: false,
   watchForFileChanges: true,
   e2e: {
-    baseUrl: `http://host.docker.internal:3000/`,
+    baseUrl: `http://host.docker.internal:5173/`,
     async setupNodeEvents(on) {
       on("before:browser:launch", (browser, launchOptions) => {
         if (browser.name === "chrome") {
@@ -22,6 +22,6 @@ export default defineConfig({
       })
     },
     specPattern: "**/*.cy.{js,jsx,ts,tsx}",
-    supportFile: "./cypress/support/index.ts"
+    supportFile: process.env.UNDER_DOCKER ? "./packages/e2e/cypress/support/index.ts" : "./cypress/support/index.ts"
   }
 })
